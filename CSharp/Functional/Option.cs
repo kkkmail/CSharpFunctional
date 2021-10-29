b5e595e60ba59125879958e5e9a2873ad3c64310
kkkmail <kkk@jfcglobal.net>
2021-10-29 19:34:41 -0400

    20211029

--- CSharp/Functional/Option.cs
+++ CSharp/Functional/Option.cs
@@ -62,6 +62,9 @@ public static class Option
 
 public static class OptionExt2
 {
+    /// <summary>
+    /// For database interop only!
+    /// </summary>
     public static Option<T> ToOption<T>(this T? value)
         where T : struct => value != null ? Some(value.Value) : None;
 
@@ -74,6 +77,9 @@ public static class OptionExt2
 
 public static class OptionExt
 {
+    /// <summary>
+    /// For database interop only!
+    /// </summary>
     public static Option<T> ToOption<T>(this T? value)
         where T : class => value != null ? Some(value) : None;
 
@@ -86,77 +92,77 @@ public static class OptionExt
     public static Option<R> Apply<T, R>
         (this Option<Func<T, R>> t, Option<T> arg)
         => t.Match(
-            () => None,
-            (func) => arg.Match(
-                () => None,
-                (val) => Some(func(val))));
+            none: () => None,
+            some: f => arg.Match(
+                none: () => None,
+                some: v => Some(f(v))));
 
 
     public static Option<R> Bind<T, R>
-        (this Option<T> optT, Func<T, Option<R>> f)
-        => optT.Match(
-            () => None,
-            (t) => f(t));
+        (this Option<T> t, Func<T, Option<R>> f)
+        => t.Match(
+            none: () => None,
+            some: v => f(v));
 
     //public static IEnumerable<R> Bind<T, R>
-    //    (this Option<T> @this, Func<T, IEnumerable<R>> func)
-    //    => @this.AsEnumerable().Bind(func);
+    //    (this Option<T> t, Func<T, IEnumerable<R>> func)
+    //    => t.AsEnumerable().Bind(func);
 
-    //public static Option<Unit> ForEach<T>(this Option<T> @this, Action<T> action)
-    //    => Map(@this, action.ToFunc());
+    //public static Option<Unit> ForEach<T>(this Option<T> t, Action<T> action)
+    //    => Map(t, action.ToFunc());
 
     public static Option<R> Map<T, R>
         (this Option.None _, Func<T, R> f)
         => None;
 
     public static Option<R> Map<T, R>
-        (this Option.Some<T> some, Func<T, R> f)
-        => Some(f(some.Value));
+        (this Option.Some<T> v, Func<T, R> f)
+        => Some(f(v.Value));
 
     public static Option<R> Map<T, R>
         (this Option<T> optT, Func<T, R> f)
         => optT.Match(
-            () => None,
-            (t) => Some(f(t)));
+            none: () => None,
+            some: v => Some(f(v)));
 
     //public static Option<Func<T2, R>> Map<T1, T2, R>
-    //    (this Option<T1> @this, Func<T1, T2, R> func)
-    //    => @this.Map(func.Curry());
+    //    (this Option<T1> t, Func<T1, T2, R> func)
+    //    => t.Map(func.Curry());
 
     //public static Option<Func<T2, T3, R>> Map<T1, T2, T3, R>
-    //    (this Option<T1> @this, Func<T1, T2, T3, R> func)
-    //    => @this.Map(func.CurryFirst());
+    //    (this Option<T1> t, Func<T1, T2, T3, R> func)
+    //    => t.Map(func.CurryFirst());
 
-    //public static IEnumerable<Option<R>> Traverse<T, R>(this Option<T> @this
+    //public static IEnumerable<Option<R>> Traverse<T, R>(this Option<T> t
     //    , Func<T, IEnumerable<R>> func)
-    //    => @this.Match(
+    //    => t.Match(
     //        () => List((Option<R>)None),
     //        (t) => func(t).Map(r => Some(r)));
 
     // utilities
 
-    //public static Unit Match<T>(this Option<T> @this, Action None, Action<T> Some)
-    //    => @this.Match(None.ToFunc(), Some.ToFunc());
+    //public static Unit Match<T>(this Option<T> t, Action None, Action<T> Some)
+    //    => t.Match(None.ToFunc(), Some.ToFunc());
 
-    internal static bool IsSome<T>(this Option<T> @this)
-        => @this.Match(
-            () => false,
-            (_) => true);
+    internal static bool IsSome<T>(this Option<T> t)
+        => t.Match(
+            none: () => false,
+            some: _ => true);
 
-    internal static T ValueUnsafe<T>(this Option<T> @this)
-        => @this.Match(
-            () => { throw new InvalidOperationException(); },
-            (t) => t);
+    internal static T ValueUnsafe<T>(this Option<T> t)
+        => t.Match(
+            none: () => { throw new InvalidOperationException(); },
+            some: t => t);
 
     public static T GetOrElse<T>(this Option<T> opt, T defaultValue)
         => opt.Match(
-            () => defaultValue,
-            (t) => t);
+            none: () => defaultValue,
+            some: t => t);
 
-    public static T GetOrElse<T>(this Option<T> opt, Func<T> fallback)
-        => opt.Match(
-            () => fallback(),
-            (t) => t);
+    public static T GetOrElse<T>(this Option<T> t, Func<T> fallback)
+        => t.Match(
+            none: () => fallback(),
+            some: t => t);
 
     //public static Task<T> GetOrElse<T>(this Option<T> opt, Func<Task<T>> fallback)
     //    => opt.Match(
@@ -165,35 +171,30 @@ public static class OptionExt
 
     public static Option<T> OrElse<T>(this Option<T> left, Option<T> right)
         => left.Match(
-            () => right,
-            (_) => left);
+            none: () => right,
+            some: _ => left);
 
     public static Option<T> OrElse<T>(this Option<T> left, Func<Option<T>> right)
         => left.Match(
-            () => right(),
-            (_) => left);
-
-    //public static Validation<T> ToValidation<T>(this Option<T> opt, Func<Error> error)
-    //    => opt.Match(
-    //        () => Invalid(error()),
-    //        (t) => Valid(t));
+            none: () => right(),
+            some: _ => left);
 
     // LINQ
 
-    public static Option<R> Select<T, R>(this Option<T> @this, Func<T, R> func)
-        => @this.Map(func);
+    public static Option<R> Select<T, R>(this Option<T> t, Func<T, R> func)
+        => t.Map(func);
 
     public static Option<T> Where<T>
-        (this Option<T> optT, Func<T, bool> predicate)
-        => optT.Match(
-            () => None,
-            (t) => predicate(t) ? optT : None);
+        (this Option<T> t, Func<T, bool> predicate)
+        => t.Match(
+            none: () => None,
+            some: v => predicate(v) ? t : None);
 
     public static Option<RR> SelectMany<T, R, RR>
-        (this Option<T> opt, Func<T, Option<R>> bind, Func<T, R, RR> project)
-        => opt.Match(
-            () => None,
-            (t) => bind(t).Match(
-                () => None,
-                (r) => Some(project(t, r))));
+        (this Option<T> t, Func<T, Option<R>> bind, Func<T, R, RR> project)
+        => t.Match(
+            none: () => None,
+            some: v => bind(v).Match(
+                none: () => None,
+                some: v1 => Some(project(v, v1))));
 }

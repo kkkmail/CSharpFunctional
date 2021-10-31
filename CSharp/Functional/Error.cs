648b8719616d37a5f05c8dc50f03928382e64e3f
kkkmail <kkk@jfcglobal.net>
2021-10-31 08:48:41 -0400

    20211031

---
+++ CSharp/Functional/Error.cs
@@ -0,0 +1,20 @@
+﻿namespace CSharp.Lessons.Functional;
+
+public record struct Error<TResult, TError> : IResult<TResult, TError>
+{
+    public bool IsOk => false;
+    internal TError ErrorResult { get; }
+    public Error(TError error) => ErrorResult = error;
+    public T Match<T>(Func<TResult, T> ok, Func<TError, T> error) => error(ErrorResult);
+    public override string ToString() => $"Error({ErrorResult})";
+
+    public IEnumerator<TResult> AsResultEnumerable()
+    {
+        yield break;
+    }
+
+    public IEnumerator<TError> AsErrorEnumerable()
+    {
+        yield return ErrorResult;
+    }
+}
--- CSharp/Functional/Ok.cs
+++ CSharp/Functional/Ok.cs
@@ -4,17 +4,17 @@ public record struct Ok<TResult, TError> : IResult<TResult, TError>
 {
     public bool IsOk => true;
     internal TResult OkResult { get; }
-
     public Ok(TResult result) => OkResult = result;
     public T Match<T>(Func<TResult, T> ok, Func<TError, T> error) => ok(OkResult);
-
-    public IEnumerator<TError> AsErrorEnumerable()
-    {
-        yield break;
-    }
+    public override string ToString() => $"Ok({OkResult})";
 
     public IEnumerator<TResult> AsResultEnumerable()
     {
         yield return OkResult;
+    }
+
+    public IEnumerator<TError> AsErrorEnumerable()
+    {
+        yield break;
     }
 }
